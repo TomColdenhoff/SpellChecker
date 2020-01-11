0c6807c598fcbd5b7ca3a381ce476d9030d5db19
tom-coldenhoff <t.coldenhoff@resoftware.nl>
2020-01-12 00:08:56 +0100

    Add Xcode C++ Project

---
+++ SpellChecker/SpellChecker/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SpellChecker
+//
+//  Created by Tom Coldenhoff on 12/01/2020.
+//  Copyright © 2020 Tom Coldenhoff. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
