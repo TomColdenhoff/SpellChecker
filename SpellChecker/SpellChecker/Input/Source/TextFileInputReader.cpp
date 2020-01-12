406d1f3fbe60f7bb7679958d4eb1b6a51903a4e1
tom-coldenhoff <t.coldenhoff@resoftware.nl>
2020-01-12 22:48:41 +0100

    Add TextFileInputReader

--- SpellChecker/SpellChecker/Input/Source/TextFileInputReader.cpp
+++ SpellChecker/SpellChecker/Input/Source/TextFileInputReader.cpp
@@ -7,11 +7,30 @@
 //
 
 #include "TextFileInputReader.hpp"
+#include <iostream>
+#include <fstream>
 
 namespace spellchecker::input {
 
 std::vector<std::string> TextFileInputReader::GetInput(std::string input) {
-    return std::vector<std::string>();
+    
+    std::ifstream file;
+    file.open(input);
+    
+    if (!file.is_open()) {
+        std::cout << "Couldn't open file: " << input << ". Check if the file exists" << std::endl;
+        
+        return std::vector<std::string>();
+    }
+    
+    std::vector<std::string> inputStringsVector;
+    
+    std::string word;
+    while (file >> word) {
+        inputStringsVector.push_back(word);
+    }
+    
+    return inputStringsVector;
 }
     
 }
