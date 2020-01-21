a94ab98ae467c7f6cc9468c7a4417c139443985a
Tom Coldenhoff <t.coldenhoff@resoftware.nl>
2020-01-21 22:42:37 +0100

    Sample trie

--- SpellChecker/SpellChecker/SpellSuggesting/Source/SpellSuggester.cpp
+++ SpellChecker/SpellChecker/SpellSuggesting/Source/SpellSuggester.cpp
@@ -24,60 +24,76 @@ std::vector<std::string> SpellSuggester::SuggestSpelling(std::string word) {
     return suggestions;
 }
     
+//std::vector<std::string> SpellSuggester::FindSuggestion(std::vector<char> trace, std::string remainingChars, int currentOffset, models::WordDictionaryNode* currentNode) {
+//
+//    std::vector<std::string> words;
+//
+//    models::WordDictionaryNode* initalNode = currentNode;
+//
+//    if (currentOffset > 2) {
+//        return words;
+//    }
+//
+//    for (int i = 0; i < currentNode->Children.size(); ++i) {
+//
+//        int currentNodeChar = currentNode->GetIndexChar(i);
+//
+//        if (currentNodeChar == remainingChars[0]) {
+//
+//            for (int charIndex = 0; charIndex < remainingChars.size(); ++charIndex) {
+//
+//                if (currentNode->Children[currentNode->GetCharIndex(remainingChars[charIndex])] != nullptr) {
+//
+//                    currentNode = currentNode->Children[currentNode->GetCharIndex(remainingChars[charIndex])];
+//                    trace.push_back(remainingChars[charIndex]);
+//
+//                    continue;
+//                }
+//                else if (currentOffset <= 2) {
+//
+//                    std::string remainingString;
+//                    if (charIndex == remainingChars.size() - 1) {
+//                        remainingString = remainingChars[remainingChars.size() - 1];
+//                    }
+//                    else {
+//                        remainingString = remainingChars.substr(charIndex + 1, remainingChars.size());
+//                    }
+//
+//                    std::vector newWords = FindSuggestion(trace, remainingString, currentOffset + 1, currentNode);
+//
+//                    if (newWords.size() > 1) {
+//
+//                    }
+//
+//                    words.insert(words.end(), newWords.begin(), newWords.end());
+//                }
+//            }
+//        }
+//        else {
+//            if (currentOffset <= 2 && remainingChars.size() != 0) {
+//                std::vector newWords = FindSuggestion(trace, remainingChars.substr(1, remainingChars.size()), currentOffset + 1, currentNode);
+//
+//                if (newWords.size() > 1) {
+//
+//                }
+//
+//                words.insert(words.end(), newWords.begin(), newWords.end());
+//            }
+//        }
+//    }
+//
+//    if (currentNode->IsWord && currentOffset <= 2 && initalNode == currentNode) {
+//        words.emplace_back(trace.begin(), trace.end());
+//    }
+//
+//    return words;
+//}
+    
 std::vector<std::string> SpellSuggester::FindSuggestion(std::vector<char> trace, std::string remainingChars, int currentOffset, models::WordDictionaryNode* currentNode) {
+
     
-    std::vector<std::string> words;
     
-    models::WordDictionaryNode* initalNode = currentNode;
-    
-    if (currentOffset > 2) {
-        return words;
-    }
-    
-    for (int i = 0; i < currentNode->Children.size(); ++i) {
-        
-        int currentNodeChar = currentNode->GetIndexChar(i);
-        
-        if (currentNodeChar == remainingChars[0]) {
-            
-            for (int charIndex = 0; charIndex < remainingChars.size(); ++charIndex) {
-                
-                if (currentNode->Children[currentNode->GetCharIndex(remainingChars[charIndex])] != nullptr) {
-                    
-                    currentNode = currentNode->Children[currentNode->GetCharIndex(remainingChars[charIndex])];
-                    trace.push_back(remainingChars[charIndex]);
-                    
-                    continue;
-                }
-                else if (currentOffset <= 2 && charIndex != remainingChars.size() - 1) {
-                    std::vector newWords = FindSuggestion(trace, remainingChars.substr(charIndex + 1, remainingChars.size()), currentOffset + 1, currentNode);
-                    
-                    if (newWords.size() > 1) {
-                        
-                    }
-                    
-                    words.insert(words.end(), newWords.begin(), newWords.end());
-                }
-            }
-        }
-        else {
-            if (currentOffset <= 2 && remainingChars.size() != 0) {
-                std::vector newWords = FindSuggestion(trace, remainingChars.substr(1, remainingChars.size()), currentOffset + 1, currentNode);
-                
-                if (newWords.size() > 1) {
-                    
-                }
-                
-                words.insert(words.end(), newWords.begin(), newWords.end());
-            }
-        }
-    }
-    
-    if (currentNode->IsWord && currentOffset <= 2 && initalNode == currentNode) {
-        words.emplace_back(trace.begin(), trace.end());
-    }
-    
-    return words;
+    return std::vector<std::string>();
 }
     
 }
